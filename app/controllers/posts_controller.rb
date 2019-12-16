87136247f6e98f49b20c84380b9934439d84837d
damiecode <damidigi@yahoo.com>
2019-12-16 23:43:12 +0100

    Create user and post model, controllers, views and migrations

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+   before_action :user_signed_in?, only: %i[new create]
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.valid?
+      @post.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :user_id)
+  end
+end
