e5f9056e320a8570534f4d05094a74d79af60002
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-12-17 17:14:37 -0300

    Fix route for posts index and user's and post's index view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
    devise_for :users, controllers: { registrations: 'users/registrations' }
    root to: "home#page"
-   resources :posts, only: [:create, :destroy, :index, :show,:new] 
+
+   resources :posts, only: [:create, :destroy, :index, :show,:new]
+   resources :users
 end
