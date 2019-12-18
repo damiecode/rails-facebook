7026f602a9ce1fb305cca8f597b5013b98fbf927
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-12-18 19:45:55 -0300

    Edit posts view to show comments and likes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_18_214846) do
+ActiveRecord::Schema.define(version: 2019_12_18_224312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,10 @@ ActiveRecord::Schema.define(version: 2019_12_18_214846) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.bigint "likes_id"
+    t.bigint "comments_id"
+    t.index ["comments_id"], name: "index_posts_on_comments_id"
+    t.index ["likes_id"], name: "index_posts_on_likes_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
