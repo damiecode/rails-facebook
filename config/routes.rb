6dd45c133bfa8a750dba4d96afd92e7f7d300dcf
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-12-24 10:51:17 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,20 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    passwords: 'users/passwords',
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
+
   root to: "posts#index"
 
-  get '/', to: 'users/sign_in'
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", registrations: 'users/registrations' }
-
-
   get 'notifications/destroy'
+  
   resources :posts, except: [:edit, :update]
   resources :users
   resources :likes, only: [:create]
   resources :comments, only: [:create]
   resources :friendships, except: [:new, :edit, :show]
-  resources :friendships do
-    collection do
-      patch :update
-    end
-  end
+
 end
