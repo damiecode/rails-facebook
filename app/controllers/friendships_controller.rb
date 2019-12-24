b2c889472eb89e937f07dc6939db2c34f8b43b4c
damiecode <damidigi@yahoo.com>
2019-12-24 16:31:49 +0100

    Update milestone-6

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,37 +1,29 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/LineLength
 class FriendshipsController < ApplicationController
   before_action :require_login
 
   def create
-    @friendship1 = Friendship.new(friendship_params)
-    @friendship1.user_id = current_user.id
-    @friendship1.sender_id = current_user.id
-    @friendship2 = Friendship.new(sender_id: @friendship1.sender_id, user_id: @friendship1.friend_id, friend_id: @friendship1.user_id)
+    @friendship = Friendship.new(friendship_params)
+    @friendship.user_id = current_user.id
 
-    if @friendship1.valid? && @friendship2.valid?
-      @friendship1.save
-      @friendship2.save
+    if @friendship.valid?
+      @friendship.save
       flash[:success] = 'Friend request sent'
       send_notice
     else
       flash[:alert] = 'Invalid friend request'
     end
-    redirect_to users_path
+    redirect_to request.referrer
   end
 
   def update
-    friendship1 = Friendship.where(id: params[:id][0])
-    friendship2 = Friendship.where(id: params[:id][1])
-    friendship1.update(confirmed: true)
-    friendship2.update(confirmed: true)
+    @friendship = Friendship.find(params[:id].to_i).update_column(:confirmed, true)
     flash[:success] = 'Request Accepted!'
     redirect_to request.referrer
   end
 
   def destroy
-    Friendship.where(id: params[:id]).destroy_all
     @friendship = Friendship.find_by(id: params[:id].to_i)
     @friendship.destroy
     flash[:success] = 'Friend removed'
@@ -48,8 +40,8 @@ class FriendshipsController < ApplicationController
     params.require(:friendship).permit(:friend_id)
   end
 
-  def friendship_update_params
-    params.require(:update_params).permit(:id)
+  def accept_params
+    params.require(:accept_params).permit(:friend)
   end
 
   def send_notice
@@ -58,4 +50,3 @@ class FriendshipsController < ApplicationController
     notice.save
   end
 end
-# rubocop:enable Metrics/LineLength
