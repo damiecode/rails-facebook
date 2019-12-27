1059525e2af85da079058df18474ed6acd4b8888
Marcos Medeiros <mvsmdrs@protonmail.com>
2019-12-26 21:33:00 -0300

    Fix omniauth sessions issues

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env['omniauth.auth'])
+    @fb_user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.find_by(email: @fb_user.email)
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
